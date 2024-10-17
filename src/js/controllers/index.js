cd50fee32564bd794c5686ce483e7b40a7348cc7
sa_me_des <eightsbinary@gmail.com>
2024-10-17 14:19:13 +0700

    add: controller entry point

---
+++ src/js/controllers/index.js
@@ -0,0 +1,6 @@
+import AuthController from "./AuthController";
+import LoginController from "./LoginController";
+
+const controllers = { AuthController, LoginController };
+
+export default controllers;
