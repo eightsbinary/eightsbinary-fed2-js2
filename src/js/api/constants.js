da5499dfbd19f1a1e9822d352e1cbe16f2f904e1
sa_me_des <eightsbinary@gmail.com>
2024-10-17 12:37:20 +0700

    update: add content type and reference to .env on API_KEY

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -1,6 +1,6 @@
 // Use Postman, or JavaScript to get your API key
 // In Workflow we will learn how to secure this information
-export const API_KEY = "replace-with-your-key";
+export const API_KEY = process.env.API_KEY;
 
 export const API_BASE = "https://v2.api.noroff.dev";
 
@@ -17,3 +17,5 @@ export const API_SOCIAL = `${API_BASE}/social`;
 export const API_SOCIAL_POSTS = `${API_SOCIAL}/posts`;
 
 export const API_SOCIAL_PROFILES = `${API_SOCIAL}/profiles`;
+
+export const CONTENT_TYPE_JSON = 'application/json';
