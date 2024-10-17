51dfd999183d42b16b08b33dc02775b7c5f73945
sa_me_des <eightsbinary@gmail.com>
2024-10-17 14:22:35 +0700

    update: handle login form with controller

--- src/js/router/views/auth/login.js
+++ src/js/router/views/auth/login.js
@@ -1,5 +1,10 @@
-// import { onLogin } from "../../../ui/auth/login";
+import controllers from '../../../controllers/index';
 
 const form = document.forms.login;
 
-form.addEventListener("submit", onLogin);
+if (form) {
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    controllers.LoginController.onLogin(event);
+  });
+}
