6e567e1a3addacf499884b4c1d63002a23a922c7
sa_me_des <eightsbinary@gmail.com>
2024-10-17 14:17:58 +0700

    update: using vite import env value

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -1,6 +1,6 @@
 // Use Postman, or JavaScript to get your API key
 // In Workflow we will learn how to secure this information
-export const API_KEY = process.env.API_KEY;
+export const API_KEY = import.meta.env.API_KEY;
 
 export const API_BASE = "https://v2.api.noroff.dev";
 
