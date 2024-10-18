5df5cb47d49b120f841577c2832558996a0e7818
sa_me_des <eightsbinary@gmail.com>
2024-10-18 15:15:02 +0700

    add: get url params utils

--- src/js/utilities/utils.js
+++ src/js/utilities/utils.js
@@ -3,6 +3,11 @@ const utils = {
     if (typeof path !== 'string') throw new Error('path must be a string');
     window.location.href = path;
   },
+
+  getUrlParams: (param) => {
+    const urlParams = new URLSearchParams(window.location.search)
+    return urlParams.get(param)
+  }
 };
 
 export default utils;
