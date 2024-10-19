5f0c69f13226e61b28fd7284aa84eeab439b6080
sa_me_des <eightsbinary@gmail.com>
2024-10-19 23:39:14 +0700

    add: profileController to entry point

--- src/js/controllers/index.js
+++ src/js/controllers/index.js
@@ -2,14 +2,15 @@ import AuthController from './AuthController';
 import LoginController from './LoginController';
 import LogoutController from './LogoutController';
 import PostController from './PostController';
+import ProfileController from './ProfileController';
 import RegisterController from './RegisterController';
-PostController;
 
 const controllers = {
   AuthController,
   LoginController,
   LogoutController,
   PostController,
+  ProfileController,
   RegisterController,
 };
 
